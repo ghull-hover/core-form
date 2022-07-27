940e82c56ae6867139a60ca0bd4f9a853331ee70
ghull-hover <geoff.hull@hover.to>
2022-07-27 13:23:25 -0700

    form submits to salesforce

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ export default function App() {
     company: "",
     phone: "",
     city: "",
-    state: ""
+    state: "",
+    oid: "",
+    retURL: ""
 
   });
 
   const [submitted, setSubmitted] = useState(false);
   const [valid, setValid] = useState(false);
+  const requestOptions = {
+    method: 'POST',
+    mode: 'no-cors'
+  }
 
   const handleFirstNameInputChange = (event) => {
     event.persist();
@@ -72,15 +78,25 @@ export default function App() {
     }));
   }
 
+  
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     if(values.firstName && values.lastName && values.email && values.company && values.phone && values.state) {
         setValid(true);
-        console.log(valid);
-        console.log(submitted);
+        values.oid = document.getElementsByName('oid')[0].defaultValue;
+        values.retURL = document.getElementsByName('retURL')[0].defaultValue;
+
     }
     setSubmitted(true);
     }
+
+    fetch(`https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8&oid=${values.oid}&retURL=${values.retURL}&first_name=${values.firstName}&last_name=${values.lastName}&company=${values.company}&city=${values.city}&email=${values.email}&phone=${values.phone}&=state=${values.state}`,
+    requestOptions
+    )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
 
 
   return (
@@ -89,6 +105,8 @@ export default function App() {
       <form className="register-form" onSubmit={handleSubmit}>
       {submitted && valid ? <div className="success-message">Success! Thank you for submitting. </div> : null}
 
+      <input type="hidden" name="oid" value="00D8Z000001uRTB"></input>
+      <input type="hidden" name="retURL" value="http://hover.to"></input>
       
         <input
           value={values.firstName}
