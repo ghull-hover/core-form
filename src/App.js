0378185936ecc150729f0fdc9462ba11276da037
ghull-hover <geoff.hull@hover.to>
2022-07-27 08:36:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,93 @@
-import React from "react";
+import React, {useState} from "react";
 import "./index.css";
 
 export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: ""
+
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleFirstNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      firstName: event.target.value,
+    }));
+  }
+
+  const handleLastNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      lastName: event.target.value,
+    }));
+  }
+
+  const handleEmailInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      email: event.target.value,
+    }));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(values.firstName && values.lastName && values.email) {
+        setValid(true);
+        console.log(valid);
+        console.log(submitted);
+    }
+    setSubmitted(true);
+    }
+
+
   return (
-    <div class="form-container">
-      <form class="register-form">
-        {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
+    <div className="form-container">
+      
+      <form className="register-form" onSubmit={handleSubmit}>
+      {submitted && valid ? <div className="success-message">Success! Thank you for submitting. </div> : null}
+
+      
         <input
+          value={values.firstName}
+          onChange={handleFirstNameInputChange}
           id="first-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="First Name"
-          name="firstName"
-        />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="first-name-error">Please enter a first name</span> */}
+          name="firstName" />
+          {submitted && !values.firstName ? <span>Please enter a first name</span> : null }
+          
         <input
+          value={values.lastName}
+          onChange={handleLastNameInputChange}
           id="last-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="last-name-error">Please enter a last name</span> */}
+        {submitted && !values.lastName ? <span>Please enter a last name</span> : null }
+
         <input
+          value={values.email}
+          onChange={handleEmailInputChange}
           id="email"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Email"
           name="email"
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="email-error">Please enter an email address</span> */}
-        <button class="form-field" type="submit">
+        {submitted && !values.email ? <span>Please enter an email</span> : null }
+
+
+        <button className="form-field" type="submit">
           Register
         </button>
       </form>
